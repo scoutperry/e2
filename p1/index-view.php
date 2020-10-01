4ab56c53eb27ec1e965c513e74edee92fa625f09
Scout Perry <scout.perry@gmail.com>
2020-10-01 08:06:31 -0400

    Fixed li close errors

--- p1/index-view.php
+++ p1/index-view.php
@@ -7,19 +7,19 @@
     <h1>Project One: Matching Pennies</h1>
     <h2>Mechanics</h2>
     <ul>
-        <li>Player A picks either odd or even and Player B defaults to the opposing choice.<li>
+        <li>Player A picks either odd or even and Player B defaults to the opposing choice.</li>
         <li>Both players flip a penny.</li>
         <li>If both pennies match the result is even, the player that chose even wins.</li>
         <li>If the pennies land on different sides the result is odd and the player that chose odd wins.</li>
     </ul>
     <h2>Results</h2>
     <ul>
-        <li>Player A chooses <?php echo $playerApick ?><li>
-        <li>Player B chooses <?php echo $playerBpick ?> (by default)<li>
-        <li>Player A flips their penny. Player A gets <?php echo $APennyLands ?><li>
-        <li>Player B flips their penny. Player B gets <?php echo $BPennyLands ?><li>
-        <li>The result from the pennies is <?php echo $result ?><li>
-        <li>The winner is <?php echo $winner ?><li>
+        <li>Player A chooses <?php echo $playerApick ?></li>
+        <li>Player B chooses <?php echo $playerBpick ?> (by default)</li>
+        <li>Player A flips their penny. Player A gets <?php echo $APennyLands ?></li>
+        <li>Player B flips their penny. Player B gets <?php echo $BPennyLands ?></li>
+        <li>The result from the pennies is <?php echo $result ?></li>
+        <li>The winner is <?php echo $winner ?></li>
     </ul>
 </body>
 </html>
